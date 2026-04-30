6bcb4286974a5faa8b54e99927b0261160504afa
matthewchavis8 <matthewchavis8@gmail.com>
2026-04-30 10:56:37 -0500

    added inlay hints to nvim

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,15 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.supports_method "textDocument/inlayHint" then
+      vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    end
+  end,
+})
+
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local servers = { "html", "cssls" }
